5e615c251c4e2fe0f8f546aaf45c94eecc409872
Xinas <lanzani.andrea29@gmail.com>
2021-04-06 16:27:16 +0200

    Add Pin functionalities

--- src/components/Pin.js
+++ src/components/Pin.js
@@ -1,5 +1,14 @@
 import React from "react";
 
 class Pin extends React.Component {
-
+  render() {
+    return (
+      <div>
+        <button onClick={this.props.onClick}>
+          <img src={this.props.image}/>
+          <p>{this.props.children}</p>
+        </button>
+      </div>
+    );
+  }
 }
