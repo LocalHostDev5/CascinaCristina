2798c5a790be83f258798f64f333b0774359caae
LorenzoTrick <57558456+LorenzoTrick@users.noreply.github.com>
2021-05-03 16:38:16 +0200

    Working Pan/Pinch/Zoom controls
    Lanzani please stop using "= ? :" ifs, my brain is too small to comprehend them

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Container, Row, Col, Image, ToggleButtonGroup, ToggleButton } from 'react-bootstrap';
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Room from './Room';
 import './Map.css';
 
-import floor0 from '../img/floor_1_local.png';
-import floor1 from '../img/floor_2_local.png';
+import floor0 from '../img/floor_1.jpg';
+import floor1 from '../img/floor_2.jpg';
 
 export default class Map extends React.Component {
   constructor(props) {
@@ -25,23 +26,29 @@ export default class Map extends React.Component {
     return (
       <Container fluid>
 
-        <Container fluid className="map text-center">
-          <Image id="test" src={this.state.img} />
+        <Container fluid className="map w-75 text-center">
+			<TransformWrapper>
+				<TransformComponent>
+					<React.Fragment>
+					  <Image id="test" src={this.state.img} />
 
-          {
-            this.state.img === floor0 ?
-              <div>
-                <Room icon="shower" title="Doccia">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum maiores, sapiente consequuntur voluptates quisquam ipsam dolorem error commodi aliquam, est non quasi, aut reprehenderit porro officia odio! Necessitatibus, animi iusto.
-                </Room>
-              </div>
-              :
-              <div>
-                <Room icon="extension" title="Ricreativa">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta eum odio quos. Facilis, labore voluptatem fuga eum aperiam iste, quae aliquam aut sit itaque accusantium ab laudantium repudiandae esse nihil.
-                </Room>
-              </div>
-          }
+					  {
+						this.state.img === floor0 ?
+						  <div>
+							<Room icon="shower" title="Doccia">
+							  Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum maiores, sapiente consequuntur voluptates quisquam ipsam dolorem error commodi aliquam, est non quasi, aut reprehenderit porro officia odio! Necessitatibus, animi iusto.
+							</Room>
+						  </div>
+						  :
+						  <div>
+							<Room icon="extension" title="Ricreativa">
+							  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta eum odio quos. Facilis, labore voluptatem fuga eum aperiam iste, quae aliquam aut sit itaque accusantium ab laudantium repudiandae esse nihil.
+							</Room>
+						  </div>
+					  }
+					</React.Fragment>
+				</TransformComponent>
+			</TransformWrapper>
         </Container>
 
         <div className="stairs">
