b9d4a59963e16479aaf9ecf36ebd9e8eb595c4cf
LorenzoTrick <57558456+LorenzoTrick@users.noreply.github.com>
2021-05-25 18:01:51 +0200

    Added rooms, updated floormaps to real ones
    All ground floor rooms and most first floor rooms added, with name and placeholder icon.

---
+++ client/src/src/components/Room.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Button, Modal } from 'react-bootstrap';
+
+import './Room.css';
+import '../index.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default class Room extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { show: false };
+  }
+
+  handleShow() {
+    this.setState({
+      show: true
+    });
+  }
+
+  handleClose() {
+    this.setState({
+      show: false
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="room"
+          variant="primary" onClick={this.handleShow.bind(this)}
+          style={{
+            top: this.props.x,
+            left: this.props.y
+          }}>
+          <div class="material-icons-round">
+            {this.props.icon}
+          </div>
+        </div>
+
+
+        <Modal show={this.state.show} onHide={this.handleClose.bind(this)}>
+          <Modal.Header closeButton>
+            <Modal.Title>{this.props.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>{this.props.children}</Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={this.handleClose.bind(this)}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+}
